16df820088b7a8ebb5cf6e9dffb2ce6f63ba2e93
Arman Awan <armandoesstuff@gmail.com>
2023-02-15 21:46:48 +0000

    Polish

--- Assets/Scripts/Bindy/Display/Objects/RadialContentBlock.cs
+++ Assets/Scripts/Bindy/Display/Objects/RadialContentBlock.cs
@@ -1,7 +1,9 @@
+using Bindy.Data;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 namespace Bindy.Display
@@ -10,10 +12,46 @@ namespace Bindy.Display
     {
         [SerializeField] Image mainImage;
         [SerializeField] TMP_Text mainText;
-        public void SetContent(Sprite s, int index)
+        [SerializeField] RectTransform mainRectTransform;
+        public void SetContent(int index)
         {
-            mainImage.sprite = s;
+            if (RadialPage.sprites[index] != null)
+                mainImage.sprite = RadialPage.sprites[index];
+            else
+                StartCoroutine(SetContent_Croute(index));
             mainText.text = index.ToString();
+        }
+
+        IEnumerator SetContent_Croute(int index)
+        {
+            int id = UnityEngine.Random.Range(0, 4000);
+            //Get the image url based on the index
+            UnityWebRequest request = UnityWebRequestTexture.GetTexture(DataManager.jsonData[id].thumbnailUrl);
+            //Request and error check
+            yield return request.SendWebRequest();
+            if (request.error != null)
+                Debug.LogError(request.error);
+            else //If no error, set image
+            {
+                //Get texture
+                var tex = ((DownloadHandlerTexture)request.downloadHandler).texture;
+                //Create Sprite from texture
+                Sprite sprite = Sprite.Create(tex,
+                new Rect(0, 0, tex.width, tex.height),
+                Vector2.one / 2);
+                //Set sprite
+                RadialPage.sprites[index] = sprite;
+                mainImage.sprite = RadialPage.sprites[index];
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            float xPos = Mathf.Abs(transform.position.x) / 3f;
+            var size = (1 - xPos) + 0.2f;
+            mainRectTransform.localScale = new Vector2(size, size);
+            mainRectTransform.position = new Vector2(mainRectTransform.position.x, -xPos);
+
         }
     }
 }
